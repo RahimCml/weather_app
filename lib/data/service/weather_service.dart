4f014b60ed2861d4837e898626f08667dee99d9c
Abdurrahim <abdurrahimjamalzada@gmail.com>
2023-04-25 01:15:23 +0400

    Create weather model

--- lib/data/service/weather_service.dart
+++ lib/data/service/weather_service.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 import 'package:weather_app/constants/api_constants.dart';
+import 'package:weather_app/data/models/weather_fetch_model.dart';
 
 class WeatherService {
   Future<dynamic> fetchWeatherInfo(String cityName) async {
@@ -13,6 +14,10 @@ class WeatherService {
 
     if(response.statusCode == 200) {
       final result = json.decode(response.body);
+
+      return WeatherFetchModel.fromJson(result);
+    } else {
+      throw Exception();
     }
   }
 }
